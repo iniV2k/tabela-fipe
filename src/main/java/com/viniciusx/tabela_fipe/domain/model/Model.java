4488b6000d89f56612f8dd08adb2c195d0afbb38
Vinicius Ribeiro <vini.silvaa2002@gmail.com>
2026-03-27 11:11:49 -0300

    feat/refactor: implement user flux in the search method and personalize toString of Record classes

--- src/main/java/com/viniciusx/tabela_fipe/domain/model/Model.java
+++ src/main/java/com/viniciusx/tabela_fipe/domain/model/Model.java
@@ -2,7 +2,12 @@ package com.viniciusx.tabela_fipe.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.viniciusx.tabela_fipe.domain.gateway.Identifiable;
+import org.jspecify.annotations.NonNull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Model(String name,String code) implements Identifiable {
+    @Override
+    public @NonNull String toString() {
+        return "%s | ID [%s]".formatted(name, code);
+    }
 }
--- src/main/java/com/viniciusx/tabela_fipe/main/Run.java
+++ src/main/java/com/viniciusx/tabela_fipe/main/Run.java
@@ -1,8 +1,12 @@
 package com.viniciusx.tabela_fipe.main;
 
+import com.viniciusx.tabela_fipe.domain.model.Brand;
+import com.viniciusx.tabela_fipe.domain.model.Model;
 import com.viniciusx.tabela_fipe.domain.model.VehicleType;
+import com.viniciusx.tabela_fipe.domain.model.Year;
 import com.viniciusx.tabela_fipe.service.SearchVehicleService;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Run {
@@ -12,12 +16,37 @@ public class Run {
         System.out.println("[1] Carro");
         System.out.println("[2] Moto");
         System.out.println("[3] Caminhão");
-        System.out.print("Digite o código da opção desejada: ");
+        System.out.print("Digite o ID da opção desejada: ");
 
         try(Scanner sc = new Scanner(System.in)) {
             System.out.print("Digite o tipo de veículo a ser consultado: ");
             VehicleType vehicleType = VehicleType.fromPath(sc.nextLine().trim());
 
+            List<Brand> listBrands = searchVehicleService.listBrands(vehicleType);
+            listBrands.forEach(System.out::println);
+
+            System.out.print("Filtre a busca: ");
+            searchVehicleService.filterSearch(listBrands, sc.nextLine().trim()).forEach(System.out::println);
+
+            System.out.print("Digite o ID da marca a ser consultada: ");
+            String codeBrand =  sc.nextLine().trim();
+
+            List<Model> listModels = searchVehicleService.listModels(vehicleType,  codeBrand);
+            listModels.forEach(System.out::println);
+
+            System.out.print("Filtre a busca: ");
+            searchVehicleService.filterSearch(listModels, sc.nextLine().trim()).forEach(System.out::println);
+
+            System.out.print("Digite o ID do modelo a ser consultado: ");
+            String codeModel = sc.nextLine().trim();
+
+            List<Year> years = searchVehicleService.listYears(vehicleType, codeBrand, codeModel);
+            years.forEach(System.out::println);
+
+            System.out.print("Digite o ID do ano referência: ");
+            String codeYear = sc.nextLine().trim();
+
+            System.out.println(searchVehicleService.getVehicle(vehicleType, codeBrand, codeModel, codeYear));
         }
 
     }
