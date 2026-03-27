a5a9b255fdd444096a6fbd227253fff708d5d294
Vinicius Ribeiro <vini.silvaa2002@gmail.com>
2026-03-27 10:40:50 -0300

    refactor: remove unnecessary commented code

--- src/main/java/com/viniciusx/tabela_fipe/main/Run.java
+++ src/main/java/com/viniciusx/tabela_fipe/main/Run.java
@@ -1,12 +1,8 @@
 package com.viniciusx.tabela_fipe.main;
 
-import com.viniciusx.tabela_fipe.domain.model.Brand;
-import com.viniciusx.tabela_fipe.domain.model.Model;
 import com.viniciusx.tabela_fipe.domain.model.VehicleType;
-import com.viniciusx.tabela_fipe.domain.model.Year;
 import com.viniciusx.tabela_fipe.service.SearchVehicleService;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Run {
@@ -19,24 +15,9 @@ public class Run {
         System.out.print("Digite o código da opção desejada: ");
 
         try(Scanner sc = new Scanner(System.in)) {
-            String vehicleTypeStr = sc.nextLine();
-            VehicleType vehicleType = VehicleType.fromPath(vehicleTypeStr);
-            List<Brand> brands = searchVehicleService.listBrands(vehicleType);
-            List<Model> models = searchVehicleService.listModels(vehicleType, "56");
-            List<Year> years = searchVehicleService.listYears(vehicleType, "56", "8942");
-            years.forEach(System.out::println);
-            System.out.println(searchVehicleService.getVehicle(vehicleType, "56", "8942", "2022-5"));
+            System.out.print("Digite o tipo de veículo a ser consultado: ");
+            VehicleType vehicleType = VehicleType.fromPath(sc.nextLine().trim());
 
-
-//            System.out.print("Digite o código da marca desejada: ");
-//            String codeBrand = sc.nextLine();
-//            searchVehicleService.listModels(vehicleType, codeBrand);
-//            System.out.print("Digite o código do modelo desejado: ");
-//            String codeModel = sc.nextLine();
-//            searchVehicleService.listYears(vehicleType, codeBrand, codeModel);
-//            System.out.print("Digite o código do ano desejado: ");
-//            String codeYear = sc.nextLine();
-//            searchVehicleService.listVehicle(vehicleType, codeBrand, codeModel, codeYear);
         }
 
     }
--- src/main/java/com/viniciusx/tabela_fipe/service/SearchVehicleService.java
+++ src/main/java/com/viniciusx/tabela_fipe/service/SearchVehicleService.java
@@ -38,35 +38,11 @@ public class SearchVehicleService {
         return getList(fipeVehicleClient.searchBrands(vehicleType), Brand.class);
     }
 
-//    public void listBrands(VehicleType vehicleType) {
-//        String json = fipeVehicleClient.searchBrands(vehicleType);
-//        getList(json, Brand.class);
-//    }
-
-//    public void listModels(VehicleType vehicleType, String codeBrand) {
-//        String json = fipeVehicleClient.searchModels(vehicleType, codeBrand);
-//        getList(json, Model.class);
-//    }
-
     public List<Model> listModels(VehicleType vehicleType, String codeBrand) {
         return getList(fipeVehicleClient.searchModels(vehicleType, codeBrand), Model.class);
     }
 
-//    public void listYears(VehicleType vehicleType, String codeBrand, String codeYear) {
-//        String json = fipeVehicleClient.searchYears(vehicleType, codeBrand, codeYear);
-//        getList(json, Year.class);
-//    }
-
     public List<Year> listYears(VehicleType vehicleType, String codeBrand, String codeYear) {
         return getList(fipeVehicleClient.searchYears(vehicleType, codeBrand, codeYear), Year.class);
     }
-
-//    public void listVehicle(VehicleType vehicleType, String codeBrand, String codeModel, String codeYear) {
-//        String json = fipeVehicleClient.searchVehicle(vehicleType, codeBrand, codeModel, codeYear);
-//        getVehicle(json);
-//    }
-
-//    public List<Vehicle> listVehicle(VehicleType vehicleType, String codeBrand, String codeModel, String codeYear) {
-//        return fipeVehicleClient.searchVehicle();
-//    }
 }
